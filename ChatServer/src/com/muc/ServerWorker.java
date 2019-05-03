735b97578836864a872920296843b7337b116a9f
Amatawa <sylar6smith@gmail.com>
2019-05-03 11:44:22 -0600

    Update commands

--- ChatServer/src/com/muc/ServerWorker.java
+++ ChatServer/src/com/muc/ServerWorker.java
@@ -1,7 +1,9 @@
 package com.muc;
 
+import org.apache.commons.lang3.StringUtils;
 import java.io.*;
 import java.net.Socket;
+
 
 public class ServerWorker extends Thread {
 
@@ -29,13 +31,25 @@ public class ServerWorker extends Thread {
         BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
         String line;
         while (( line = reader.readLine()) != null) {
-            if ("quit".equalsIgnoreCase(line))
-                break;
-            String msg = "You typed " + line + "\n";
-            outputStream.write(msg.getBytes());
+            String[] tokens = StringUtils.split(line);
+            if (tokens != null && tokens.length > 0) {
+                String cmd = tokens[0];
+                if ("quit".equalsIgnoreCase(line)) {
+                    break;
+                } else if ("login".equalsIgnoreCase(cmd)) {
+                    handleLogin(outputStream,tokens);
+                }else {
+                    String msg = "unknown" + cmd + "\n";
+                    outputStream.write(msg.getBytes());
+                }
+                String msg = "You typed " + line + "\n";
+                outputStream.write(msg.getBytes());
+            }
         }
-
         clientSocket.close();
+    }
+
+    private void handleLogin(OutputStream outputStream, String[] tokens) {
     }
 }
 
